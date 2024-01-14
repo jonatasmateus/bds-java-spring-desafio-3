ceeebc09c5b549cc35e85b7d0659d688a3302066
Jonatas <jonatasmateuscontato@gmail.com>
2024-01-14 17:25:39 -0300

    Database seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Helena Alves Santana', '003.647.400-27', 6700.00, '1998-10-20', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Noah Brito Souza', '992.924.460-37', 7000.00, '2001-01-28', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Miguel Carvalho Souza', '570.412.720-79', 4200.00, '2002-07-13', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Luna Ribeiro Alves', '732.604.640-93', 8700.00, '1999-11-11', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucas Machado Agostini', '009.005.920-44', 3000.00, '2003-04-02', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Marcelo Alves Brito Cavalcante', '214.444.330-08', 12100.00, '1991-03-10', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Rafaela Lima Alencar', '542.725.120-05', 2300.00, '2003-01-31', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Déborah Cristine Souza', '899.820.010-43', 3600.00, '2003-05-14', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ágatha Taylor Silva', '493.781.170-31', 4900, '2000-06-08', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Hugo Santos Brandão', '052.734.500-80', 20000.00, '1986-09-12', 2);
