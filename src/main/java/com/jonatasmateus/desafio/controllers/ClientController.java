2a9808211978525b80efa360d6e07d059016893a
Jonatas <jonatasmateuscontato@gmail.com>
2024-01-14 21:31:43 -0300

    ClientController annotations adjusted

--- src/main/java/com/jonatasmateus/desafio/controllers/ClientController.java
+++ src/main/java/com/jonatasmateus/desafio/controllers/ClientController.java
@@ -19,12 +19,12 @@ public class ClientController {
         return service.findById(id);
     }
 
-    @GetMapping()
+    @GetMapping
     public Page<ClientDTO> findAll(Pageable pageable) {
         return service.findAll(pageable);
     }
 
-    @PostMapping()
+    @PostMapping
     public ClientDTO insert(@RequestBody ClientDTO dto) {
         return service.insert(dto);
     }
