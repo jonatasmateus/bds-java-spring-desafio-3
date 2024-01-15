7f43cebc9ff1ea244a9e097f024bc90424193c12
Jonatas <jonatasmateuscontato@gmail.com>
2024-01-14 21:22:37 -0300

    POST new client implemented

--- src/main/java/com/jonatasmateus/desafio/services/ClientService.java
+++ src/main/java/com/jonatasmateus/desafio/services/ClientService.java
@@ -25,4 +25,19 @@ public class ClientService {
         Page<Client> result = repository.findAll(pageable);
         return result.map(ClientDTO::new);
     }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+        passDtoToEntity(dto, entity);
+        return new ClientDTO(repository.save(entity));
+    }
+
+    private void passDtoToEntity(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+    }
 }
